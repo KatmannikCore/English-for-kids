c3c3721cc44dcdd06ef85fbfa09634811132003e
KatmannikCore <73733601+KatmannikCore@users.noreply.github.com>
2021-11-08 12:50:28 +0300

    Made cards

--- js/cards.js
+++ js/cards.js
@@ -1,5 +1,5 @@
 import { cards, topics, cardsCategories } from "./configs/cardsConfig.js";
-let blocks = document.querySelectorAll(".block");
+let blocks = document.querySelectorAll(".cardFront");
 function Temp(){
     let menu = document.querySelectorAll(" li a ");
     for(let i = 0; i < menu.length; i++){
@@ -14,8 +14,25 @@ function SetCart(cards){
     for(let j = 0; j < images.length; j++){
         images[j].src = cards[j].image;
     }
-    let words = document.querySelectorAll(".word p ");
+    let words = document.querySelectorAll(".cardFront");
     for(let j = 0; j < words.length; j++){
         words[j].innerHTML = cards[j].word;
     }
+    let translationWords= document.querySelectorAll(".cardBack");
+    for(let j = 0; j < words.length; j++){
+        translationWords[j].innerHTML = cards[j].translation;
+    }
+    let musics= document.querySelectorAll("audio");
+    for(let i = 0; i < musics.length; i ++){
+        musics[i].src = cards[i].audioSrc;
+    }
 }
+
+document.querySelectorAll(".card").forEach(item => item.addEventListener('click', function(){
+    this.classList.toggle('applyflip');
+    //this.onmouseover = this.querySelector("audio").play();
+}))
+
+document.querySelectorAll(".card").forEach(item => item.addEventListener('mouseenter', function(event){
+    item.querySelector("audio").play()
+}))
