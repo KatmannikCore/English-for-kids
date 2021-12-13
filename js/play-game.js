815f524683b53da38bd789cbb4859828f5c15c61
KatmannikCore <vlad.urbanovitch@gmail.com>
2021-12-13 12:00:02 +0300

    relese

--- js/play-game.js
+++ js/play-game.js
@@ -6,7 +6,7 @@ document.querySelector(".slider").onclick = function(){
         document.querySelector(".start-game").hidden = false ;
         document.querySelectorAll(".card").forEach(item => item.removeEventListener('mouseenter', FlipCart));
         document.querySelectorAll(".card").forEach(item => item.removeEventListener('click', PlayMusic));
-        document.querySelectorAll(".cardStatic img").forEach(item => item.style.maxWidth =  'none'); 
+        document.querySelectorAll(".card img").forEach(item => item.style.height = "180px");
         CheckTrue()
 
     }
@@ -19,39 +19,46 @@ document.querySelector(".start-game").onclick = function(){
     document.querySelector(".start-game").hidden = true ;
 }
 let arrayMusic = [];
-let randomElement;
 let countErrors = 0;
+let currentWord = 0;
+
 document.querySelectorAll(".card audio").forEach(item => arrayMusic.push(item)) 
+shuffle(arrayMusic)
 document.querySelector(".game").onclick= function(){
     if(arrayMusic.length == 0){
         EndGame();
     }else{
-        randomElement = arrayMusic[Math.floor(Math.random() * arrayMusic.length)];
-        document.querySelector(".audio-game").src = randomElement.src;
-        randomElement.play();
+        document.querySelector(".audio-game").src = arrayMusic[currentWord].src;
+        arrayMusic[currentWord].play();
     }
 }
- 
+function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+  }
 
 function CheckTrue(){
     document.querySelectorAll(".card").forEach(item => item.addEventListener('click',AddImg))
 }
 function AddImg(event){
-    if(event.currentTarget.querySelector("audio").src == document.querySelector(".audio-game").src){
-        event.currentTarget.style.opacity = "0.33"
-        stars.insertAdjacentHTML("beforeend",'<img src="../assets/icons/star-win.svg" alt="">');
-        DeleteElementArrayMusic()
-    }else{
-        stars.insertAdjacentHTML("beforeend",'<img src="../assets/icons/star.svg" alt="">');
-        countErrors++;
-    }
-    if(arrayMusic.length == 0){
-        EndGame();
+    if(event.currentTarget.style.opacity != "0.33"){
+        if(event.currentTarget.querySelector("audio").src == document.querySelector(".audio-game").src){
+            event.currentTarget.style.opacity = "0.33"
+            stars.insertAdjacentHTML("beforeend",'<img src="../assets/icons/star-win.svg" alt="">');
+            DeleteElementArrayMusic()
+            arrayMusic[currentWord].play();
+        }else{
+            stars.insertAdjacentHTML("beforeend",'<img src="../assets/icons/star.svg" alt="">');
+            countErrors++;
+            
+        }
+        if(arrayMusic.length == 0){
+            EndGame();
+        }
     }
 }
 function DeleteElementArrayMusic(){
     for(let i = 0; i < arrayMusic.length; i++){
-        if(arrayMusic[i] == randomElement){
+        if(arrayMusic[i] == arrayMusic[currentWord]){
             arrayMusic.splice(i, 1);
         }
     }    
@@ -67,9 +74,9 @@ function Clean(){
     document.querySelector(".start-game").hidden = true ;
     document.querySelectorAll(".card").forEach(item => item.removeEventListener('click',AddImg))
     document.querySelectorAll(".card").forEach(item => item.addEventListener('mouseenter', FlipCart));
-    document.querySelectorAll(".card").forEach(item => item.addEventListener('click', PlayMusic));
-    document.querySelectorAll(".cardStatic img").forEach(item => item.style.maxWidth =  '100%'); 
+    document.querySelectorAll(".card").forEach(item => item.addEventListener('click', PlayMusic)); 
     document.querySelectorAll(".card").forEach(item => item.style.opacity = "1");
+    document.querySelectorAll(".card img").forEach(item => item.style.height = "");
     document.querySelector(".game").hidden = true ;
 }
 document.querySelector('.modal').addEventListener('click',()=>location.reload())
